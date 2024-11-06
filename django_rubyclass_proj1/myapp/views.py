ec8c0c3fe411f5ef2467e2d0ccc4e20ebedd638d
Nyabayo <ernestosindo9@gmail.com>
2024-11-06 13:22:56 +0300

    webinar3

--- django_rubyclass_proj1/myapp/views.py
+++ django_rubyclass_proj1/myapp/views.py
@@ -6,3 +6,9 @@ def home(request):
     return render(request,'index.html')
 def about(request):
     return render(request,'about.html')
+def contact(request):
+    return render(request, "contacts.html")
+def services(request):
+    return render(request, "services.html")
+def blogs(request):
+    return render(request, "blogs.html")
